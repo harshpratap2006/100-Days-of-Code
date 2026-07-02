cb431c814f063c9df060c6b57f87844377e93eb3
Harsh Pratap <kushwahaharshit702@gmail.com>
2026-07-02 23:35:01 +0530

    day 6

--- day 6/remove_duplicate.c
+++ day 6/remove_duplicate.c
@@ -20,25 +20,33 @@ Explanation: Keep first occurrence of each element: 1, 2, 3*/
 
 #include <stdio.h>
 
-int main(){
+int main() {
     int n;
     scanf("%d", &n);
 
     int arr[n];
 
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    // Print unique elements
-    if(n > 0){  
-        printf("%d ", arr[0]); // Print the first element
+    if (n == 0) {
+        return 0;
+    }
 
-        for(int i = 1; i < n; i++){
-            if(arr[i] != arr[i - 1]){ // Check for duplicates
-                printf("%d ", arr[i]);
-            }
+    int j = 0; // Index of last unique element
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] != arr[j]) {
+            j++;
+            arr[j] = arr[i];
         }
     }
+
+    // Print unique elements
+    for (int i = 0; i <= j; i++) {
+        printf("%d ", arr[i]);
+    }
+
     return 0;
 }
