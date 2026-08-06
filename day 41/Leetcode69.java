9b7576fc4e8c1bc6b9ea77040081b1e9cb7a6e41
Harsh Pratap <kushwahaharshit702@gmail.com>
2026-08-06 23:54:46 +0530

    day 41

---
+++ day 41/Leetcode69.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Leetcode69 {
+
+    public static int mySqrt(int x) {
+
+        if (x == 0 || x == 1) {
+            return x;
+        }
+
+        int left = 1;
+        int right = x;
+        int ans = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Use long to avoid integer overflow
+            long square = (long) mid * mid;
+
+            if (square == x) {
+                return mid;
+            } else if (square < x) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input
+        int x = sc.nextInt();
+
+        // Find square root
+        int result = mySqrt(x);
+
+        // Output
+        System.out.println(result);
+
+        sc.close();
+    }
+}
