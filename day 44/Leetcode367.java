6fa973d01a6aed85ba4da5d23f9fac255f9e8aee
Harsh Pratap <kushwahaharshit702@gmail.com>
2026-08-09 23:47:22 +0530

    day 43

---
+++ day 44/Leetcode367.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Leetcode367 {
+
+    public static boolean isPerfectSquare(int num) {
+
+        int left = 1;
+        int right = num;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            long square = (long) mid * mid;
+
+            if (square == num) {
+                return true;
+            } 
+            else if (square < num) {
+                left = mid + 1;
+            } 
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Input
+        int num = sc.nextInt();
+
+        // Check whether num is a perfect square
+        boolean result = isPerfectSquare(num);
+
+        // Output
+        System.out.println(result);
+
+        sc.close();
+    }
+}
