c8569805d165a2e272e82cda757c8b80fbdd5ef8
Harsh Pratap <kushwahaharshit702@gmail.com>
2026-08-29 23:33:26 +0530

    day 64

---
+++ day 64/SetMatrixZero.java
@@ -0,0 +1,68 @@
+public class SetMatrixZero {
+
+    public static void setZeroes(int[][] matrix) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
+
+        // Find rows and columns containing zero
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+
+        // Set marked rows to zero
+        for (int i = 0; i < m; i++) {
+            if (row[i]) {
+                for (int j = 0; j < n; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Set marked columns to zero
+        for (int j = 0; j < n; j++) {
+            if (col[j]) {
+                for (int i = 0; i < m; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    // Function to print matrix
+    public static void printMatrix(int[][] matrix) {
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+            {1, 1, 1},
+            {1, 0, 1},
+            {1, 1, 1}
+        };
+
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+
+        setZeroes(matrix);
+
+        System.out.println("\nMatrix after setting zeroes:");
+        printMatrix(matrix);
+    }
+}
